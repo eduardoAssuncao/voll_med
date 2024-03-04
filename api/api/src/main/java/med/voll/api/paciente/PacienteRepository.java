c926e8b9871fee49325aec9ad41af8f7e051dbf4
Eduardo (Kenny8855) Assunção <39742355+eduardoAssuncao@users.noreply.github.com>
2024-03-04 09:16:15 -0300

    Add files via upload

---
+++ api/api/src/main/java/med/voll/api/paciente/PacienteRepository.java
@@ -0,0 +1,9 @@
+package med.voll.api.paciente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, String> {
+  
+}
